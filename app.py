f62d49ea33883448ff6140a422230fb9766fb506
hugo99-code <hugo99.lindbarg@gmail.com>
2025-06-13 14:34:24 +0200

    Lagt till psycopg2 och dotenv, uppdaterat databasanslutning

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from dotenv import load_dotenv
+load_dotenv()
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -9,7 +11,7 @@ app.secret_key = 'supersecretkey'  # Du kan ersätta med en säkrare nyckel i en
 database_url = os.environ.get('DATABASE_URL')
 if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
